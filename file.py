ddeca056516df6ea39b1f74f7bb3b518d1262439
peerfahad <fahadrashid611@gmail.com>
2020-04-27 01:35:09 +0500

    connecting the tesseract path and showing the img

--- file.py
+++ file.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
+import pytesseract
 
+pytesseract.pytesseract.tesseract cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+text = pytesseract.image_to_string(img)
 img = cv2.imread("image.jpg")
 
 cv2.imshow("img",img)
